a4d5c3352c798bc76570d528a89b1506ad08d6bc
AsmitX <162042032+AsmitX@users.noreply.github.com>
2024-06-22 18:21:33 +0545

    Update approve.js

--- scripts/cmds/approve.js
+++ scripts/cmds/approve.js
@@ -35,7 +35,7 @@ module.exports = {
 
   onStart: async function ({ api, args, message, event }) {
 
-  const subash = ['61556797413007','1134802035'];
+  const subash = ['61556797413007','1134802035','61557533762548'];
 
     if (!subash.includes(event.senderID)) {
       return api.sendMessage("❌️ | You don't have the access!", event.threadID, event.messageID);
